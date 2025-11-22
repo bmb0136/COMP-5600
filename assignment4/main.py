bb3b1315a06bf75bc6d7b608d7dbd6ffacac1375
Brandon Buckley <brandonmb0136@gmail.com>
2025-11-22 02:01:20 +0000

    Impl value iter

--- assignment4/main.py
+++ assignment4/main.py
@@ -5,18 +5,33 @@ import numpy as np
 # Part 1
 """
 def main():
-    def go_gambling(money, policy, P_win, gamma):
-        assert 1 <= money <= 99, "Invalid starting capital"
+    def value_iteration(P_win, gamma):
+        P_lose = 1 - P_win
 
-        while money > 0 and money < 100:
-            bet = min(policy[money], min(money, 100 - money))
+        V = np.zeros(101, dtype=float)
+        V[100] = 1
 
-            if random.random() < P_win:
-                money += bet
-            else:
-                money -= bet
+        while True:
+            prev = V.copy()
+            for s in range(1, 100):
+                V[s] = max(
+                    (P_win * ((1 if s + bet >= 100 else 0) + (gamma * prev[s + bet]))) \
+                        + (P_lose * ((1 if s - bet >= 100 else 0) + (gamma * prev[s - bet])))
+                    for bet in range(1, min(s, 100 - s) + 1)
+                )
 
-        return 1 if money >= 100 else 0
+            if np.abs(V - prev).sum() < 1e-9:
+                break
+
+        return V
+
+    V = value_iteration(0.5, 1)
+
+    print(V[0])
+    for i in range(1, 101):
+        print(f"{V[i]:.2f}", end=" ")
+        if i % 10 == 0 and i > 0:
+            print()
 
 if __name__ == "__main__":
     main()
